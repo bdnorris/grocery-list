f16b47bc2d711181d9fd58dd3ee0571526abd7f3
Brian Norris <brian.norris@acxion.com>
2025-03-04 15:54:37 -0600

    typescript

--- src/components/EmojiPicker.vue
+++ src/components/EmojiPicker.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="emoji">
-    {{ emoji.emoji }}
+    {{ emoji }}
   </div>
 </template>
 
@@ -17,13 +17,20 @@ export default {
     }
   },
   setup (props) {
-    function getEmoji (name: string) {
+    interface Emoji {
+      emoji: string,
+      name: string
+      group: string,
+      sub_group: string,
+      codepoints: string
+    }
+    function getEmoji () {
       const wordsInItemName = props.itemName.split(' ').map((word: string) => word.toLowerCase());
       // create an array with the original array, but also remove any plurals and add those as well
       const wordsInItemNamePluralAndSingular = [...wordsInItemName, ...wordsInItemName.map((word: string) => pluralToSingular(word))];
       console.log('wordsInItemNamePluralAndSingular:', wordsInItemNamePluralAndSingular);
       // search the emoji data for any matching characters in the wordsInItemName array
-      const emojis = emojiData.map((item: { emoji: string, name: string }) => {
+      const emojis: any = emojiData.map((item: { emoji: string, name: string }) => {
         const nameWords = item.name.split(' ');
         const match = nameWords.filter((word: string) => wordsInItemNamePluralAndSingular.includes(word));
         if (match.length > 0) {
@@ -31,11 +38,15 @@ export default {
         }
       }).filter((item: any) => item);
       console.log('emoji:', emojis);
-      return emojis ? emojis[0] : false;
+      if (emojis.length > 0) {
+        return emojis[0].emoji;
+      } else {
+        return '';
+      }
     }
     return {
       localName: props.itemName,
-      emoji: getEmoji(props.itemName)
+      emoji: getEmoji()
     }
   }
 }
